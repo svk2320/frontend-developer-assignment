3dd7898224ae4a035ad7c107c62c4da95fcef4db
username <vasanthkumars2025@gmail.com>
2023-04-03 14:29:01 +0530

    added styles

--- src/components/navigation-bar/navigation-bar.component.jsx
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -98,14 +98,14 @@ const NavigationBar = () => {
                     <div className={styles.title1}>Privacy Policy</div>
                     <div className={styles.instanceItem} />
                   </div>
-                  <div className={styles.titleParent2}>
+                  {/* <div className={styles.titleParent2}>
                     <div className={styles.title1}>FAQs</div>
                     <div className={styles.instanceItem} />
                   </div>
                   <div className={styles.titleParent2}>
                     <div className={styles.title1}>Smallcase</div>
                     <div className={styles.instanceItem} />
-                  </div>
+                  </div> */}
                 </div>
                 <div className={styles.instanceParent1}>
                   <div className={styles.titleParent2}>
@@ -156,12 +156,12 @@ const NavigationBar = () => {
                       <div className={styles.instanceItem} />
                     </div>
                   </div>
-                </div>
+                </div> */}
               </div>
             </div>
-            <div className={styles.registerNowWrapper}>
+            <button className={styles.registerNowWrapper}>
               <div className={styles.contactNow}>Register Now</div>
-            </div>
+            </button>
             <img className={styles.topBarItem} alt="" src="/group-36901.svg" />
             <div className={styles.titleWrapper}>
               <div className={styles.title}>Go back to my profile</div>
